f96844384a8f45d24eb953f966ca7d2c1aff8920
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-05-18 14:45:47 +0530

    currency-project/  custom Hook is created for api call and dynamic component also creates <InputBox>

---
+++ currency-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import InputBox from './components/InputBox'
+// import useCurrencyInfo from './hooks/useCurrencyInfo';
+
+const App = () => {
+  return (
+    <div className=''>
+      {/* <InputBox/> */}
+    </div>
+  )
+}
+
+export default App
