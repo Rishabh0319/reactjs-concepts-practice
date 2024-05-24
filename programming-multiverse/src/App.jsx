de63fe8a36e1c9185abeecaa70322039ede00dde
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-05-24 14:46:02 +0530

    programming-multiverse/  Project is Done all Data render properly in card components :)

--- programming-multiverse/src/App.jsx
+++ programming-multiverse/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Card from './components/Card';
+import data from "./database/data";
 
 const App = () => {
+  const cardDataList = data;
   return (
     <div className='flex items-center justify-start flex-wrap gap-4 p-4'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {
+        data.map((card, index) => {
+          return <Card key={index}
+           language={card.name}
+            year={card.year}
+            creator={card.creator}
+            useCase={card.usecase}
+          />
+        })
+      }
     </div>
   )
 }
