8d04cca459026b22714280d0c1a9878121b12780
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-05-24 15:39:07 +0530

    image-gallery/  UI part is Done now next is logical and state management part

---
+++ image-gallery/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const App = () => {
+  const images = [
+    'https://imgs.search.brave.com/bAdI7a7qnuuOIJphttt3DfbzLnCU7ZDMtnkejXq6UII/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdDIu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMDkz/OC81NDk5L2kvNDUw/L2RlcG9zaXRwaG90/b3NfNTQ5OTg2MTMt/c3RvY2stcGhvdG8t/Z2luZ2VyLWNhdC5q/cGc',
+    'https://imgs.search.brave.com/YzG9FgYN5qLWsiYyJ4dUEvxav9e98dBH0loR8YPRA1E/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTc0/ODc3NTY1L3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtYnJvd24tY2F0/LWFnYWluc3QtYS1n/cmF5LWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWlGcFFNSzlF/aTIzVm9XcExLa2Zh/TFVtdXN5Y3VaWllV/OWtWMjNzT2F6YzQ9',
+    'https://imgs.search.brave.com/lZtf1S7JKFcaZs2lhxTpAtaJzTk_V35Xt8ys4htuVBU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/Y2F0LXBvc2VzLXBl/cmZlY3RseS5qcGc_/d2lkdGg9MTAwMCZm/b3JtYXQ9cGpwZyZl/eGlmPTAmaXB0Yz0w',
+    'https://imgs.search.brave.com/t6Nv0DwxoIACRxxtX2h7yt31ux5SCXHWgHVpoGh1diw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzM2Lzk5LzIy/LzM2MF9GXzIzNjk5/MjI4M19zTk94Q1ZR/ZUZMZDVwZHFhS0do/OERSR01aeTdQNFhL/bS5qcGc',
+  ];
+  return (
+    <div>
+      <div className='flex flex-col items-center justify-between'>
+        <h1 className='text-blue-600 underline text-sm'>Click on an image</h1>
+        <div className='flex flex-wrap items-center justify-center w-[90%] rounded-md p-2 gap-2'>
+          {
+            images.map((image, index) => (
+              <div key={index} className='w-1/5 border border-gray-300 rounded-md cursor-pointer hover:scale-105 transition-all'>
+                <img className='h-[100%] w-[100%] bg-cover' src={image} alt="" />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      <div className='flex flex-col items-center justify-between'>
+        <h1 className='font-bold text-2xl'>Selected Image</h1>
+        <div className='w-[40%] overflow-hidden border border-gray-500 rounded-md'>
+          <img className='w-[100%] h-[100%] bg-cover' src={images[0]} alt="" />
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default App
