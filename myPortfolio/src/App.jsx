7ced0864419b5974ca926053586272a21f6350be
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-06-12 02:22:30 +0530

    myPortfolio/  Nav section of my-portfolio is Done

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
+import Hero from './components/Hero/Hero.jsx';
 
 import Navbar from "./components/Navbar/Navbar.jsx";
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ myPortfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+       <div className={styles.content}>
+          <h1 className={styles.title}>Hello, I'm Rishabh</h1>
+          <p className={styles.description}>I'm fullstack Developer with 1 year of Experience of React.js, Node.js, Express.js and mongoDB. Reach out if you'd like to learn more</p>
+          <a href="mailto:rishabhtripathi0319@gmail.com" className={styles.contactBtn}>Contact Me</a>
+       </div>
+       <img src={getImageUrl("hero/heroImage.png")} className={styles.heroImg} alt="Hero" />
+       <div className={styles.topBlur}/>
+       <div className={styles.bottomBlur}/>
+    </section>
+  )
+}
+
+export default Hero
