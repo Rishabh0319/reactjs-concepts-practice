64aef7f1a37a9a6f6d4dd8ba7e68f1af1891174e
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-06-15 10:05:37 +0530

    myPortfolio/  Experiance section is done

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from './components/Hero/Hero.jsx';
 import About from './components/About/About.jsx';
+import Experience from './components/Experience/Experience.jsx';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Experience/>
     </div>
   )
 }
