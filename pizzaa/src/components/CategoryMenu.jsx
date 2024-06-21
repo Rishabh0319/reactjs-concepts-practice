ee1e823da467dabbb350d73c71b83acd229c946f
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-06-22 01:28:41 +0530

    pizzaa/  Nav Filter category features is done

--- pizzaa/src/components/CategoryMenu.jsx
+++ pizzaa/src/components/CategoryMenu.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import FoodData from '../data/foodData';
+import {useDispatch,useSelector} from 'react-redux';
+import { setCategory } from '../redux/slices/categorySlice';
 
 const CategoryMenu = () => {
+
+  const [categories,setCategories] = useState([]);
+
+  const listUniqueCategories = ()=>{
+    const uniqueCategories = [...new Set(FoodData.map(item=>item.category))]
+    setCategories(uniqueCategories);
+    // console.log(uniqueCategories);
+  }
+
+  useEffect(()=>{
+    listUniqueCategories();
+  },[])
+
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector(state=>state.category.category);
+
   return (
     <div className='mx-6'>
         <h2 className='my-3 text-orange-500 font-semibold text-3xl'>find The Best Food</h2>
         <div className='my-5 flex gap-3 overflow-x-auto scroll-smooth'>
-        <button className='px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out'>All</button>
-        <button className='px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out'>Lunch</button>
-        <button className='px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out'>Breakfast</button>
-        <button className='px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out'>Dinner</button>
-        <button className='px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out'>Snacks</button>
+        <button onClick={()=>dispatch(setCategory('All'))} className={`px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out ${selectedCategory === "All" && "bg-green-500 text-white"}`}>All</button>
+        {
+          categories.map((category,index)=><button key={index} onClick={()=>dispatch(setCategory(category))} className={`px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out ${selectedCategory === category && "bg-green-500 text-white"}`}>{category}</button>)
+        }
         </div>
     </div>
   )
--- pizzaa/src/components/FoodItems.jsx
+++ pizzaa/src/components/FoodItems.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
-import FoodCard  from './FoodCard';
+import FoodCard from './FoodCard';
 import foodData from '../data/foodData';
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+
 
 const FoodItems = () => {
-  const handleToast = (name)=>toast.success(`Added ${name}`);
+
+  const handleToast = (name) => toast.success(`Added ${name}`);
+  const category = useSelector(state => state.category.category);
+
   return (
     <>
-    <Toaster position='top-center' reverseOrder={false}/>
-    <div className='flex flex-wrap items-center justify-center gap-8 mx-6 my-10 lg:justify-start'>
-      {
-        foodData.map((food)=>(
-          <FoodCard key={food.id}
-            id={food.id}
-            name={food.name}
-            price={food.price}
-            description={food.desc}
-            rating={food.rating}
-            img={food.img}
-            handleToast={handleToast}
-          />
-        ))
-      }
-    </div>
+      <Toaster position='top-center' reverseOrder={false} />
+      <div className='flex flex-wrap items-center justify-center gap-8 mx-6 my-10 lg:justify-start'>
+        {
+          foodData.filter((food) => {
+            if (category === "All") {
+              return food;
+            } else {
+              return category === food.category
+            }
+          }).map((food) => (
+            <FoodCard key={food.id}
+              id={food.id}
+              name={food.name}
+              price={food.price}
+              description={food.desc}
+              rating={food.rating}
+              img={food.img}
+              handleToast={handleToast}
+            />
+          ))
+        }
+      </div>
     </>
   )
 }
