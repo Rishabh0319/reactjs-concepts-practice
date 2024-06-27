b2ccd7b30030ee98c54241098bf70fa6f9c40516
Rishabh0319 <rishabhtripathi0319@gmail.com>
2024-06-27 12:30:11 +0530

    pizzaa/  add some Data to webapp project is Done

--- pizzaa/src/components/CategoryMenu.jsx
+++ pizzaa/src/components/CategoryMenu.jsx
@@ -22,7 +22,7 @@ const CategoryMenu = () => {
 
   return (
     <div className='mx-6'>
-        <h2 className='my-3 text-orange-500 font-semibold text-3xl'>find The Best Food</h2>
+        <h2 className='my-3 text-orange-500 font-semibold text-3xl'>Find The Best Pizza</h2>
         <div className='my-5 flex gap-3 overflow-x-auto scroll-smooth'>
         <button onClick={()=>dispatch(setCategory('All'))} className={`px-3 py-2 bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer duration-500 ease-in-out ${selectedCategory === "All" && "bg-green-500 text-white"}`}>All</button>
         {
